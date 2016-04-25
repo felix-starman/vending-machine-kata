583a67c4c851c82e49aa9430807ea8ce702910f5
Peter Mueller <pmueller@covermymeds.com>
2016-04-24 22:31:47 -0400

    add coin_return spec & code, rubocop suggestions

--- lib/vending_machine.rb
+++ lib/vending_machine.rb
@@ -1,34 +1,38 @@
 class VendingMachine
-  attr_reader :amount_inserted
+  attr_reader :amount_inserted, :coin_return
 
   VALID_COINS = [
-    { value: 25, weight: 5.670, diameter: 24.26,},
-    { value: 10, weight: 2.267, diameter: 17.91,},
-    { value: 5, weight: 5.000, diameter: 21.21,},
+    { value: 25, weight: 5.670, diameter: 24.26 },
+    { value: 10, weight: 2.267, diameter: 17.91 },
+    { value: 5, weight: 5.000, diameter: 21.21 }
   ]
 
   def initialize
     @amount_inserted = 0
+    @coin_return = []
+    @inserted_coins = []
   end
 
   def insert_coin(coin)
     if valid_coin? coin
-      @amount_inserted += VALID_COINS.select do |vc|
-        compare_coin vc, coin
+      @amount_inserted += VALID_COINS.select do |valid|
+        compare_coin valid, coin
       end[0][:value]
       true
     else
+      @coin_return << coin
       false
     end
   end
 
   def valid_coin?(coin)
-    VALID_COINS.one? { |vc| compare_coin vc, coin }
+    VALID_COINS.one? { |valid| compare_coin valid, coin }
   end
 
   private
 
-  def compare_coin reference, subject_coin
-    reference[:weight] == subject_coin.weight && reference[:diameter] == subject_coin.diameter
+  def compare_coin(reference, subject_coin)
+    reference[:weight] == subject_coin.weight &&
+      reference[:diameter] == subject_coin.diameter
   end
 end
--- spec/vending_machine_spec.rb
+++ spec/vending_machine_spec.rb
@@ -4,22 +4,22 @@ describe VendingMachine do
   let(:nickel)  { CoinHelper.new weight: 5.000, diameter: 21.21 }
   let(:penny)   { CoinHelper.new weight: 2.500, diameter: 19.05 }
 
-  subject { described_class.new() }
+  subject { described_class.new }
 
   describe '#valid_coin?' do
     [:quarter, :dime, :nickel].each do |coin|
       it "returns true for a #{coin}" do
-        expect(subject.valid_coin?(public_send coin)).to eq true
+        expect(subject.valid_coin?(public_send(coin))).to eq true
       end
     end
 
     it 'returns false for a penny' do
-      expect(subject.valid_coin? penny).to eq false
+      expect(subject.valid_coin?(penny)).to eq false
     end
   end
 
   describe '#insert_coin' do
-    let(:coin) { }
+    let(:coin) { nil }
     let(:vending_machine) { described_class.new }
 
     subject { vending_machine.insert_coin coin }
@@ -30,7 +30,7 @@ describe VendingMachine do
       it { is_expected.to eq true }
 
       it 'increases the amount_inserted' do
-        expect { subject }.to change{vending_machine.amount_inserted}.by(25)
+        expect { subject }.to change { vending_machine.amount_inserted }.by(25)
       end
     end
 
@@ -40,7 +40,12 @@ describe VendingMachine do
       it { is_expected.to eq false }
 
       it 'does not increase the amount_inserted' do
-        expect { subject }.not_to change{vending_machine.amount_inserted}
+        expect { subject }.not_to change { vending_machine.amount_inserted }
+      end
+
+      it 'sends the coin to the coin return' do
+        expect { subject }.to change { vending_machine.coin_return }.from([])
+          .to([coin])
       end
     end
   end
